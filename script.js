866259db421b1fda4878fb93df585f7d329d4a93
Alexander Kummerow <alex@kmrw.de>
2025-04-17 22:12:32 +0200

    Suche optimiert

--- script.js
+++ script.js
@@ -66,3 +66,30 @@ fetch('http://localhost:3000/daten')
   .catch(error => {
     console.error('Fehler beim Laden der Daten:', error);
   });
+
+function search() {
+
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("search");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("kartentabelle");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, hide all first and then unhide those who match the search query
+  for (i = 1; i < tr.length; i++) {
+    // Hide the row initially.
+    tr[i].style.display = "none";
+
+    td = tr[i].getElementsByTagName("td");
+    for (var j = 0; j < td.length; j++) {
+      cell = tr[i].getElementsByTagName("td")[j];
+      if (cell) {
+        if (cell.innerHTML.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+          break;
+        } 
+      }
+    }
+}
+}
